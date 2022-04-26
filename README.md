ef18f462b36f0e7451aa9364301b27592c20ad00
FullStackDeveloper-Kiran <fullstackdeveloperkiran11@gmail.com>
2022-04-26 14:17:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Login Folding to Terminal
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/josetxu/pen/WNoZRNe](https://codepen.io/josetxu/pen/WNoZRNe).
+
+_Created for the [February 2021 CodePen Challenge](https://codepen.io/challenges/2021/February)_
---
+++ dist/script.js
@@ -0,0 +1,28 @@
+var warnIng = document.getElementById('warning');
+var errOr = document.getElementById('error');
+var userName = document.getElementById('user');
+var passWord = document.getElementById('pass');
+var logIn = document.getElementById('log-in');
+
+logIn.addEventListener('click', function() {
+	if(userName.value != '' && passWord.value != ''){
+		var allPnl = this.parentNode.parentNode.parentNode.children;
+		for(var i=0; i<allPnl.length; i++){
+			allPnl[i].classList.add('fold-'+(i+1));						
+		}
+		this.parentNode.parentNode.parentNode.classList.add('hide');
+		document.getElementById('user-logged').innerText = userName.value;
+		document.getElementById('user-name').innerText = userName.value;
+		document.getElementById('date').innerText = new Date().toDateString();
+		document.querySelector('textarea').focus();
+		document.querySelector('.t-login').style.display='none';
+		document.querySelector('.t-terminal').style.display='inline-block';
+	} else {
+		warnIng.classList.add('show');
+		if(passWord.value == ''){ errOr.innerText = ' Password'; }
+		if(userName.value == ''){ errOr.innerText = ' Username'; }
+		warnIng.addEventListener('click', function() {
+			warnIng.classList.remove('show');
+		});
+	}
+});
